20cb4259cd71fd97960bc434dcf34f43636b9bfb
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-04-02 20:39:59 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Funcion que se ejecuta al hacer click en el boton
+const getPosts = async () => {
+    try {
+        const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const resultado = await respuesta.json();
+
+        if (resultado.ok === false) {
+            throw new Error('Error en la petición');
+        }
+
+        const container = document.getElementById('post-data');
+
+        // Limpiar el contenedor
+        container.innerHTML = '';
+
+        resultado.forEach(post => {
+            const listaDesordenada = document.createElement('ul');
+            listaDesordenada.innerHTML = `
+                <li><h2>${post.title}</h2></li>
+                <p>${post.body}</p>
+            `;
+            container.appendChild(listaDesordenada);
+        });
+
+    } catch (error) {
+        console.log("Error en la petición", error);
+    }
+}
